7b4f25b261cb812b63a3e2423ead2e7e1fb9432d
ishmael <goodridgeishmael@gmail.com>
2025-11-12 12:33:58 -0500

    ish cal

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
 
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero")
+    return b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    if a = 0:
+        raise ValueError("No log base 0")
+    return math.log(b, a)
+    #loga(b) use math library + raise ValueError
+
+def exp(a, b):
+    return math.pow(a,b)
 
