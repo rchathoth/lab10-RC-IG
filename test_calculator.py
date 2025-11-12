899cb32fcea6d74bbf9e12d7d4d2655c0691fe3d
Your Name <you@example.com>
2025-11-12 12:58:36 -0500

    last step

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,16 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+         self.assertEqual(add(1, 2), 3)
+         self.assertEqual(add(1, 1000), 1001)
+         self.assertEqual(add(-1, 2), 1)
+
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(1, 2), -1)
+        self.assertEqual(sub(100, 2), 98)
+        self.assertEqual(sub(0, 2), -2)
     # ##########################
 
     ######## Partner 1
@@ -19,14 +23,13 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+    #
+        self.assertRaises(div(0,5), ZeroDivisionError)
+    #    
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertRaises(log(0, 3), ValueError)
 
     # def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
