831739f0f3b7c79f519d46f3284be3ca026690de
ishmael <goodridgeishmael@gmail.com>
2025-11-12 12:25:15 -0500

    lfs;

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero")
+    return b / a
+
+def log(a, b):
+    math.log(b, a)
+    if a == 0:
+        raise ValueError("Cannot have log base of 0")
+
+def exp(a, b):
+    math.pow(a,b)
 
 
