27060027179378e95922a3508de23f95538cd951
Your Name <you@example.com>
2025-11-12 12:07:13 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,24 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if b == 0:
+        raise ZeroDivisionError("Can't divide by 0!")
+    return a / b
+
+def log(a,b):
+    if b == 0:
+        raise ValueError("Logarithm cannot take 0 as an argument!")
+    return math.log(b, a)
+
+def exp(a,b):
+    return a ** b
 
